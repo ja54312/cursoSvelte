15957df5abf475e378ba005952daa41441d82f76
JA54312 <ija54312@gmail.com>
2023-12-15 11:29:33 -0600

    styles

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,31 +1,50 @@
 <script>
-	export let name;
-	export let lastName;
+  export let name;
+  export let lastName;
+  let svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png";
 </script>
 
 <main>
-	<h1>Hello {name} {lastName}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name} {lastName}!</h1>
+  <p>FrontEnd Developer</p>
+  <img src={svelteLogo} alt="sevelte logo" />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  :global(body) {
+    background-color: #f2eee2;
+    color: #0084f6;
+  }
+  :global(:root) {
+    --theme-color: purple;
+    font-size: 22px;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  p {
+    color: var(--theme-color);
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  main img {
+    width: 30%;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
